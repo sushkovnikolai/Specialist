9876e9c2e7b6d7d9ff032725a0c59c7e6a1afb40
sushkovnikolai <46484254+sushkovnikolai@users.noreply.github.com>
2020-03-29 14:56:45 +0300

    first comit

---
+++ InputOutput/input_output4.py
@@ -0,0 +1,5 @@
+#Напишите код, который принимает целое число и печатает числа,
+# которые следуют до него и посленего.
+nam = int(input())
+print("The following number for the number " + str(nam) + " : " + str(nam +1))
+print("Previous number for number " + str(nam) + " : " + str(nam -1))
